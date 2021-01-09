d71b39c7197fe19d07f0c8b2dee0b6faceecdadf
MarleyPaniah <p.marlonbradley@gmail.com>
2021-01-09 19:48:44 +0100

    View mounting

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -4,7 +4,6 @@ import API_TOKEN from '../Helpers/token'
 
 export function getFilmsFromApiWithSearchedText(text, page) {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_TOKEN}&language=fr&query=${text}&page=${page}`;
-    console.log(url)
     return fetch(url)
         .then((response) => response.json()) // The response was corretly received and turned into a json
         .catch((error) => console.log(error)) // In case of error
@@ -13,4 +12,9 @@ export function getFilmsFromApiWithSearchedText(text, page) {
 export function getImageFromAPI(name) {
     return `https://image.tmdb.org/t/p/w300${name}`
 }
- 
+
+export function getFilmDetailFromApi(id) {
+    return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_TOKEN}&language=fr`)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+}
--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,14 +1,58 @@
 // Components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { getFilmDetailFromApi } from '../API/TMDBApi'
 
 class FilmDetail extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            film: undefined,
+            isLoading: true // we want it to load when trying to go on this view
+        }
+    }
+
+    componentDidMount() {
+        idFilm = this.props.route.params.idFilm; // parameters passed during navigation from a view to another is handled with this
+        getFilmDetailFromApi(idFilm)
+        .then(data => {
+            this.setState({
+                film: data,
+                isLoading: false
+            })
+        })
+        console.log("Component FilmDetail mounted")
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return(
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large'/>
+                </View>
+            )
+        }
+    }
+
+    _displayFilm() {
+        if (this.state.film != undefined) {
+            return (
+                <ScrollView style={styles.scrollview_container}>
+                    <Text>{this.state.film.title}</Text>
+                </ScrollView>
+            )
+        }
+    }
+
     render() {
         //console.log("PROPS", this.props)
         return (
             <View style={styles.main_container}>
-                <Text>Détail du film {this.props.route.params.idFilm}</Text>
+                {this._displayLoading()}
+                {this._displayFilm()}
             </View>
         )
     }
@@ -16,6 +60,18 @@ class FilmDetail extends React.Component {
 
 const styles = StyleSheet.create({
     main_container: {
+        flex: 1
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    scrollview_container: {
         flex: 1
     }
 })
