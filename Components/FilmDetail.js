bef484a328f93469c53ee20e65f2d6004c2266b0
MarleyPaniah <p.marlonbradley@gmail.com>
2021-01-09 20:21:52 +0100

    Displays the film description and info when clicking on it

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,9 +1,12 @@
 // Components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View, Text, ActivityIndicator } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator, Image } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
-import { getFilmDetailFromApi } from '../API/TMDBApi'
+import { getFilmDetailFromApi, getImageFromAPI } from '../API/TMDBApi'
+
+import moment from 'moment'
+import numeral from 'numeral'
 
 class FilmDetail extends React.Component {
 
@@ -38,10 +41,28 @@ class FilmDetail extends React.Component {
     }
 
     _displayFilm() {
-        if (this.state.film != undefined) {
+        const { film } = this.state
+        if (film != undefined) {
             return (
                 <ScrollView style={styles.scrollview_container}>
-                    <Text>{this.state.film.title}</Text>
+                    <Image
+                        style={styles.image}
+                        source={{uri: getImageFromAPI(film.backdrop_path)}}
+                    />
+                    <Text style={styles.title_text}>{film.title}</Text>
+                    <Text style={styles.description_text}>{film.overview}</Text>
+                    <Text style={styles.default_text}> 
+                        Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}
+                    </Text>
+                    <Text style={styles.default_text}>Note : {film.vote_average}/10</Text>
+                    <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
+                    <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
+                    <Text style={styles.default_text}>
+                        Genre(s) : {film.genres.map(
+                            function(genre){
+                                return genre.name;
+                            }).join('/')}
+                    </Text>
                 </ScrollView>
             )
         }
@@ -60,7 +81,8 @@ class FilmDetail extends React.Component {
 
 const styles = StyleSheet.create({
     main_container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: 'lightgrey'
     },
     loading_container: {
         position: 'absolute',
@@ -73,6 +95,33 @@ const styles = StyleSheet.create({
     },
     scrollview_container: {
         flex: 1
+    },
+    image: {
+        height: 169,
+        margin: 5
+    },
+    title_text: {
+        fontWeight: 'bold',
+        fontSize: 35,
+        flex: 1,
+        flexWrap: 'wrap',
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+        marginBottom: 10,
+        color: '#000000',
+        textAlign: 'center'
+    },
+    description_text: {
+        fontStyle: 'italic',
+        color: '#666666',
+        margin: 5,
+        marginBottom: 15
+    },
+    default_text: {
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 5,
     }
 })
 
